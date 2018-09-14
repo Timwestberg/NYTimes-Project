3b044c6d050655b7ec04310b4cbccace1012a825
snowember <timwestberg43@gmail.com>
2018-09-13 21:11:20 -0700

    here is all the structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+
+var subject = $(“#searchTerm”).val();
+var limit = $(“#recordNum”).val();
+var beginDate = $(“#beginDate”).val();
+var endDate = $(“endDate”).val();
+
+var url = “https://api.nytimes.com/svc/search/v2/articlesearch.json”;
+url += ‘?’ + $.param({
+ ‘api-key’: “e84d952de99a4688814ecb57806adcc2”,
+ ‘q’: + subject,
+ ‘begin_date’: + beginDate,
+ ‘end_date’: + endDate,
+ ‘page’: + limit
+});
+$.ajax({
+ url: url,
+ method: ‘GET’,
+}).then(function(response){
+var results = response;
+console.log(results);
+
+
+
+});
